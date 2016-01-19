efa1406e7f2108027f61687068089d34bd41e27f
Ted Moore <Nermal@Live.com>
2016-01-19 14:40:37 -0500

    Version object

---
+++ BrewFindApi/src/coldcoffee/brewfind/api/Objects/Version.java
@@ -0,0 +1,23 @@
+package coldcoffee.brewfind.api.Objects;
+
+public class Version extends BrewFindObject{
+	
+	public int brewNum;
+	public int verNum;
+	
+	public int getV_brewNum(){
+		return brewNum;
+	}
+	
+	public void setV_brewNum(int brewNum){
+		this.brewNum=brewNum;
+	}
+	
+	public int getV_verNum(){
+		return verNum;
+	}
+	
+	public void setV_verNum(int verNum){
+		this.verNum=verNum;
+	}
+}
