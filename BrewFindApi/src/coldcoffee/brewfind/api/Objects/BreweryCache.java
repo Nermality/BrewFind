360d624c9c475d1aa42a070c7fcae5d8c85ff80b
MHarwood <mharwood12@live.com>
2016-01-25 17:39:56 -0500

    Basic cache methods

--- BrewFindApi/src/coldcoffee/brewfind/api/Objects/BreweryCache.java
+++ BrewFindApi/src/coldcoffee/brewfind/api/Objects/BreweryCache.java
@@ -6,32 +6,54 @@ import java.util.Map;
 
 public class BreweryCache {
 
-	// BreweryNum -> ( VersionNum -> Brewery )
-	private static Map<Integer, HashMap<Integer, Brewery>> cacheMap;
+	// BreweryNum -> Brewery
+	private static Map<Integer, Brewery> brewMap;
+	
+	// BreweryNum -> VersionNum
+	private static Map<Integer, Integer> versionMap;
 	
 	public BreweryCache() {
-		cacheMap = new HashMap<Integer, HashMap<Integer, Brewery>>();
+		brewMap = new HashMap<Integer, Brewery>();
+		versionMap = new HashMap<Integer, Integer>();
 	}
 	
-	public Map<Integer, HashMap<Integer, Brewery>> getInstance() {
-		return cacheMap;
-	}
-	
+	// initialize map
 	public void init(List<Brewery> list) {
-		// initialize map
+		
+		for( Brewery b : list) {
+			brewMap.put(b.getB_breweryNum(), b);
+			versionMap.put(b.getB_breweryNum(), b.getB_version());
+		}
 	}
 	
+	// newly updated brewery to be put into map
+	// add brewery if not already in list
 	public void update(Brewery toUpdate) {
-		// newly updated brewery to be put into map
-		// add brewery if not already in list
+		
+		if(versionMap.containsKey(toUpdate.getB_breweryNum())) {
+			int curVer = versionMap.get(toUpdate.getB_breweryNum());
+			if(curVer >= toUpdate.getB_version()) {
+				return;
+			}
+		}
+		
+		brewMap.put(toUpdate.getB_breweryNum(), toUpdate);
+		versionMap.put(toUpdate.getB_breweryNum(), toUpdate.getB_version());
 	}
 	
 	public Brewery findByNumber(int brewNum) {
+		
+		if(brewMap.containsKey(brewNum)) {
+			return brewMap.get(brewNum);
+		}
+		
 		return null;
 	}
 	
 	public void removeByNumber(int brewNum) {
 		
+		brewMap.remove(brewNum);
+		versionMap.remove(brewNum);
 	}
 	
 	public List<Brewery> updateReturnList(List<Version> clientVersion) {
