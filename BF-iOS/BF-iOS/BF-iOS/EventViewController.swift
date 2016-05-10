b4f7072378e3fce07fff1e485c1b39625f30fd04
mharwood <th3pie@gmail.com>
2016-05-09 20:19:59 -0400

    Here we go - iOS begins again.

---
+++ BF-iOS/BF-iOS/BF-iOS/EventViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BF-iOS
+//
+//  Created by Mark Harwood on 5/9/16.
+//  Copyright © 2016 Cold Coffee Studios. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BF-iOS/BF-iOS/BF-iOS/MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  BF-iOS
+//
+//  Created by Mark Harwood on 5/9/16.
+//  Copyright © 2016 Cold Coffee Studios. All rights reserved.
+//
+
+import Foundation
